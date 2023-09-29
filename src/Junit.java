aee50fcd5da8e6b486de474e58d67d35bea88915
Darren <dmeidl15@gmail.com>
2023-09-29 11:39:01 +0100

    Wrote sumDouble method, created 4 tests for them.

--- src/Junit.java
+++ src/Junit.java
@@ -1,6 +1,13 @@
 public class Junit {
 
-    public int add2Nums (int a, int b){
+    public int add2Nums(int a, int b){
         return  a + b;
+    }
+
+    public int sumDouble(int a, int b){
+        if (a == b){
+            return (a + b) * 2;
+        }
+        return a + b;
     }
 }
--- src/JunitTest.java
+++ src/JunitTest.java
@@ -3,8 +3,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JunitTest {
-
-    @Test
+    //add2Nums Method Tests
+    /*@Test
     void twoPlusTwoEqualsFour() {
         System.out.println("Test 1 - 2 + 2");
         int num1 = 2;
@@ -46,5 +46,51 @@ class JunitTest {
         int expected = -11;
         int actual = j.add2Nums(num1, num2);
         assertEquals(expected, actual);
+    }*/
+
+    //sumDouble Method Tests
+    @Test
+    void fivePlusSixEqualsEleven(){
+        System.out.println("TEST 1: 5 + 6");
+        int num1 = 5;
+        int num2 = 6;
+        Junit j = new Junit();
+        int expected = 11;
+        int actual = j.sumDouble(num1, num2);
+        assertEquals(expected, actual);
     }
+
+    @Test
+    void sixPlusSixEqualsTwentyFour(){
+        System.out.println("TEST 2: 6 + 6");
+        int num1 = 6;
+        int num2 = 6;
+        Junit j = new Junit();
+        int expected = 24;
+        int actual = j.sumDouble(num1, num2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void minusSixPlusSixEqualsZero(){
+        System.out.println("TEST 3: -6 + 6");
+        int num1 = -6;
+        int num2 = 6;
+        Junit j = new Junit();
+        int expected = 0;
+        int actual = j.sumDouble(num1, num2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void minusEightPlusMinusEightEqualsMinusSixteen(){
+        System.out.println("TEST 4: -8 + -8");
+        int num1 = -8;
+        int num2 = -8;
+        Junit j = new Junit();
+        int expected = -32;
+        int actual = j.sumDouble(num1, num2);
+        assertEquals(expected, actual);
+    }
+
 }
