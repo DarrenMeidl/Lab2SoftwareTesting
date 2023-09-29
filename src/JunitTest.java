ba57d9d9e23d45db5ecc45dc9ce6fff03cad7996
Darren <dmeidl15@gmail.com>
2023-09-29 10:42:36 +0100

    Added print & two ints to test

--- src/JunitTest.java
+++ src/JunitTest.java
@@ -5,6 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class JunitTest {
 
     @Test
-    void add2Nums() {
+    void twoPlusTwoEqualsFour() {
+        System.out.println("Test 1 - 2 + 2");
+        int num1 = 2;
+        int num2 = 2;
+
+
+
     }
 }
